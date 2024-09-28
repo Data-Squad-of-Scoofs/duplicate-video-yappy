af87946cfdcb4fbc048a36defc9a86e15c27d189
fluke8 <frist410@gmail.com>
2024-09-29 03:26:09 +0700

    make audio3

--- src/audio_analysis.py
+++ src/audio_analysis.py
@@ -38,9 +38,57 @@ def get_audio_features(model, features=None, video_folder=None, video_paths=None
         # audio_data, _ = librosa.load(audio_data, sr=48000) # sample rate should be 48000
         audio_data = audio_data.reshape(1, -1) # Make it (1,T) or (N,T)
 
+        if audio is None:
+            print(audio)
+
         with torch.no_grad():
             audio_embed = model.get_audio_embedding_from_data(x = audio_data, use_tensor=False)
 
         all_features[video_id] = torch.tensor(audio_embed)
+
+    return all_features
+
+
+def get_audio_features_rhythm(features=None, video_folder=None, video_paths=None):
+    if video_folder is not None:
+        video_paths = [os.path.join(video_folder, name) for name in os.listdir(video_folder)]
+    elif video_paths is None:
+        raise ValueError("Either video_folder or video_paths must be provided")
+
+    print("Starting to extract audio features....")    
+
+
+    if features:
+        all_features = features
+    else:
+        all_features = {}
+
+    for path in tqdm(video_paths, ncols=70):
+        video_id = os.path.basename(path.split('.')[0])
+
+        if video_id in all_features:
+            continue   
+        
+        video = VideoFileClip(path)
+        audio = video.audio
+
+        if audio is None:
+            continue
+        try:
+            fps = 7000
+
+            audio_data = audio.to_soundarray(fps=fps, quantize=True, buffersize=10000)
+            audio_data = np.mean(audio_data, axis=1)
+
+            min_length = len(audio_data)
+            original_onset_vector = np.zeros(min_length)
+            original_onsets = librosa.onset.onset_detect(y=audio_data, sr=fps, units='time')
+            original_onsets = np.array(original_onsets) * fps
+            original_onsets = original_onsets[original_onsets < min_length]
+            original_onset_vector[original_onsets.astype(int)] = 1
+
+            all_features[video_id] = torch.tensor(original_onset_vector)
+        except Exception as e:
+            print(f'Ошибка вылезла в видосе {video_id}: {e}')
 
     return all_features
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,13 @@
 import os
 import requests
+import numpy as np
 
-def download_file_from_s3(bucket_url, object_key, download_path):
+
+def download_file(object_url, download_path):
     directory = os.path.dirname(download_path)
     if not os.path.exists(directory):
         dir_path = os.path.join(os.getcwd(), directory)
         os.makedirs(dir_path)
-        
-    object_url = os.path.join(bucket_url, object_key)
 
     response = requests.get(object_url)
 
@@ -31,6 +31,8 @@ def compute_similarities(q_feat, d_feat, topk_cs=True):
     sim = sim.mean().item()
     return sim
 
+
+
 def calculate_similarities(video_id=None, second_video_id=None, all_features=None):
     if video_id is not None and second_video_id is not None:
         return compute_similarities(all_features[video_id], all_features[second_video_id])
@@ -42,8 +44,37 @@ def calculate_similarities(video_id=None, second_video_id=None, all_features=Non
         return similarities
     
     elif all_features is not None:
-        similarities = {video_id: compute_similarities(video_id, all_features=all_features) 
+        similarities = {video_id: calculate_similarities(video_id, all_features=all_features) 
                         for video_id in all_features}
+        return similarities
+
+    return None  # В случае, если не указаны необходимые переменные
+
+def compute_rhythm_similarities(q_feat, d_feat):
+    min_length = min(len(q_feat), len(d_feat))
+
+    rhythm_similarity = (np.corrcoef(q_feat[:min_length], d_feat[:min_length])[0, 1] + 1) / 2
+
+    return rhythm_similarity
+
+from tqdm import tqdm
+
+def calculate_rhythm_similarities(video_id=None, second_video_id=None, all_features=None):
+    if video_id is not None and second_video_id is not None:
+        return compute_rhythm_similarities(all_features[video_id], all_features[second_video_id])
+    
+    elif video_id is not None and all_features is not None:
+        first_feat = all_features[video_id]
+        similarities = {}
+        for second_video_id in tqdm(all_features, desc='Calculating similarities', ncols=70):
+            if second_video_id != video_id:
+                similarities[second_video_id] = compute_rhythm_similarities(first_feat, all_features[second_video_id])
+        return similarities
+    
+    elif all_features is not None:
+        similarities = {}
+        for video_id in tqdm(all_features, desc='Calculating similarities for all videos', ncols=70):
+            similarities[video_id] = calculate_rhythm_similarities(video_id, all_features=all_features)
         return similarities
 
     return None  # В случае, если не указаны необходимые переменные
